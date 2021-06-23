7981898340cec4a03c8f7d1299015af7b040e2a7
letuankiet212 <48205415+letuankiet212@users.noreply.github.com>
2021-06-23 22:37:43 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
-
-import {data} from './assets/data'
+import { useState } from "react";
+import { data } from "./assets/data";
 function App() {
+	const [numbers, setNumbers] = useState(data);
 	return (
 		<div className="App p-r h-screen">
-			<div className="container centered bg-black h-1/3 px-1.5">
+			<div className="container centered  px-1.5">
 				<div className="header flex j-space-between">
 					<div className="name">calc</div>
 					<div className="theme"></div>
 				</div>
 				<div className="main">
-					<div className="hienthi bg-blue-900 text-right color-white">
+					<div className="hienthi bg-blue-900 text-right color-white br-5">
 						399,981
 					</div>
-					<div className="mt-2 p-1.5 bg-blue-900 grid-cols-4 gap-3 grid">
-						<div className="bg-gray-200 br-5 w-full h-4 p-r">
-						</div>
-						<div className="bg-gray-200 br-5 w-full h-4">
-
-						</div>
-						<div className="bg-gray-200 br-5 w-full h-4">
-
-						</div>
-						<div className="bg-gray-200 br-5 w-full h-4">
-
-						</div>
-						<div className="bg-gray-200 br-5 w-full h-4">
-
-						</div>
+					<div className="mt-2 p-1.5 bg-blue-900 grid-cols-4 gap-3 grid br-5 btn text-base">
+						{numbers.map((number, index) => (
+							<div
+								className={
+									"bg-gray-200 br-5 w-full h-4 p-r pt-0.5" +
+									(number === "DEL" ? " color-gray-200" : "")
+								}
+								style={
+									number === "DEL"
+										? {
+												boxShadow: "0px 5px 0px #414E73",
+												backgroundColor: "#647299",
+										  }
+										: { boxShadow: "0px 5px 0px #B2A39B" }
+								}
+							>
+								<p className="centered w-full text-center">{number}</p>
+							</div>
+						))}
 					</div>
 				</div>
 			</div>
